5ef850af40c202db4e26343637f85e160fa751ab
Neo's code <su5858@vip.qq.com>
2017-03-09 12:38:16 +0800

    140度摄像头-无畸变矫正算法

---
+++ 直立程序 - 封装后 - 综合 - 无畸变矫正算法/Lib_User/inc/Car_Control.h
@@ -0,0 +1,9 @@
+#ifndef __CAR_CONTROL_H__
+#define __CAR_CONTROL_H__
+
+
+void Car_Control_Init();
+void Car_Control(float TargetSpeed);
+
+
+#endif
---
+++ 直立程序 - 封装后 - 综合 - 无畸变矫正算法/Lib_User/inc/image_processing.h
@@ -0,0 +1,10 @@
+#ifndef __IMAGE_PROCESSING_H__
+#define __IMAGE_PROCESSING_H__
+
+
+void ChouQu_ErZhi(uint8 gray[][188],uint8 binarized[][94]);
+void Scan_Edge_ShuiPing(uint8 Line[],uint8 *flag_l,uint8 *Left,uint8 *flag_r,uint8 *Right);
+void JiaoZheng(uint8 yuantuxiang[][94],uint8 jiaozhenghou[][94]);
+
+
+#endif
